4441958f1027886fbdacdba20e5b421596074e09
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2020-02-01 12:07:29 -0500

    Progress

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
 
-class MyApp extends StatelessWidget {
+////2nd Class need for adding State/////
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/picone.jpg'),
-          Text('Food Paradise')
-        ],),),
-      ),
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState((){
+                      _products.add('Advanced Foot Taster');
+                    });
+                    
+                  },
+                  child: Text('Add Product'),
+                ),
+              ),
+              Column(
+                children: _products
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/picone.jpg'),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              )
+            ],
+          )),
     );
   }
 }
